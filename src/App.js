1e657def495e0685055b94a86cd70d0494b06948
Rejin Nepal <rejinnepal815@gmail.com>
2022-09-04 16:09:27 -0500

    Edited this file to render Fi_calculator component here

--- src/App.js
+++ src/App.js
@@ -1,78 +1,10 @@
-import './index.css';
-import {useState} from 'react';
-
-function App() {
-
-  const [calc, setCalc] = useState("");
-  const [result, setResult] = useState("");
-
-  const ops = ['/', '*', '+', '-', '.'];
-
-  const updateCalc = value =>{
-    if (ops.includes(value) && calc === '' || ops.includes(value) && ops.includes(calc.slice(-1))){
-      return;
-    }
-    setCalc(calc+value);
-
-    if (!ops.includes(value)){
-      setResult(eval(calc + value).toString())
-    }
-  }
-
-  const createDigits = ()=>{
-    const digits =[];
-    for (let i = 1; i < 10; i++){
-      digits.push(<button onClick={()=>updateCalc(i.toString())} key={i}>{i}</button>)
-    }
-    return digits;
-  }
-
-  const calculate= ()=>{
-    setCalc(eval(calc).toString());
-    setResult();
-  }
-
-  const deleteLast = ()=>{
-    if (calc == ''){
-      return;
-    }
-    const value = calc.slice(0,-1);
-    setCalc(value);
-    setResult(value);
-  }
-
-  const clearHistory= ()=>{
-    const value = calc.slice(-1,-1);
-    setCalc(value);
-    setResult(0);
-  }
-
-  return (
-    <div className="App">
-      <div className="calculator">
-        <div className = "display">
-          {result ? <span>({result})</span>: ''}
-          &nbsp;
-          {calc || "0"}
-        </div>
-        <div className="operators">
-          <button onClick={()=>updateCalc('/')}>/</button>
-          <button onClick={()=>updateCalc('+')}>+</button>
-          <button onClick={()=>updateCalc('-')}>-</button>
-          <button onClick={()=>updateCalc('*')}>*</button>
-          <button onClick={()=>clearHistory()}>CE</button>
-          <button onClick={deleteLast}>Del</button>
-        </div>
-        <div className = "digits">
-          {createDigits()}
-          <button onClick={()=>updateCalc('0')}>0</button>
-          <button onClick={()=>updateCalc('.')}>.</button>
-          <button onClick={calculate}>=</button>
-        </div>
-      </div>
-
+import Fi_calculator from './Fi_calculator.js';
+function App(){
+  return(
+    <div>
+      <Fi_calculator></Fi_calculator>
     </div>
-  );
+  )
 }
 
 export default App;
